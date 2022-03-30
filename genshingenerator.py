f8828f577c56928ceb4fad55ea437ebada90467d
Reaganchandra <102659305+Reaganchandra@users.noreply.github.com>
2022-03-30 16:20:40 +0700

    Update genshingenerator.py

--- genshingenerator.py
+++ genshingenerator.py
@@ -1,9 +1,9 @@
 import json
 import requests
 
-def createAcc(username, password):
-    success = False
-    payload = "is_crypto=false&not_login=0&username="+username+"&password="+password
+def createAcc(Reagan, Reaganban123):
+    success = True
+    payload = "is_crypto=false&not_login=0&username="+Reagan+"&Reaganban123="+Reaganban123
     headers = {'Content-Type':'application/x-www-form-urlencoded'}
     req = requests.session()
     req.headers.update(headers)
@@ -15,12 +15,12 @@ def createAcc(username, password):
         print("error")
     return success    
 ignlist = open("igns.txt","r").readlines()
-password = input("Type password: ")
+password = input("Reaganban123: ")
 output = open('output.txt', 'w')
 created = []
 for x in ignlist:
-    status = createAcc(x.replace("\n",""), password)
-    account = x.replace("\n","") + ":" + password
+    status = createAcc(x.replace("\n",""), Reaganban123)
+    account = x.replace("\n","") + ":" + Reaganban123
     if status == True:
         print(account)
         output.write(account+"\n")
